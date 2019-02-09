7a2196f5d8653c5d8b766501238175841bd2b5d5
Jose Patron <patronalayola55@gmail.com>
2019-02-08 20:52:51 -0600

    Calculadora con operaciones basicas

---
+++ index.js
@@ -0,0 +1,40 @@
+const math = require("./math");
+
+console.log(math.suma(10,20));
+console.log(math.resta(10,20));
+console.log(math.multiplicacion(10,20));
+console.log(math.division(10,20));
+
+
+
+
+
+
+
+
+
+
+/*
+// CREO VARIABLE
+let nombre = "Jose"
+
+function saludar(){
+  console.log("hola " + nombre);
+}
+
+//LLAMO A LA FUNCIÓN
+saludar();
+*/
+
+
+
+
+
+/*
+Estructura básica de una función
+
+function saludar(parametro de funcion){
+}
+
+saludar();
+*/
---
+++ math.js
@@ -0,0 +1,46 @@
+
+/*
+function suma(a, b){
+  return a + b;
+}
+*/
+
+
+module.exports = {
+  suma: function (a,b) {
+    return a + b;
+  },
+  resta: function (a,b){
+    return a - b;
+  },
+  multiplicacion: function(a,b){
+    return a * b;
+  },
+  division: function(a,b){
+    return a / b;
+  }
+}
+
+
+
+
+/*
+//Ejemplo de objeto
+const persona = {
+  nombre: "Jose",  //propiedad
+  saludar: function(){  //metodo
+    console.log("hola");
+  },
+  despedirse: function(){
+    console.log("adios");
+  }
+}
+*/
+
+
+
+
+
+
+//return es para guardar
+//Dentro de un objeto no tengo que poner el nombre de la función, i.e, función anónima
